7da65e76fdcca83c6f9cdc92aa8ede647f2bd7cd
RileyA <praetor57@gmail.com>
2011-07-08 03:36:54 -0700

    Initial commit.

---
+++ src/example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// simple usage example, makes a GUI and saves the resulting mesh data to a wavefront .obj file
+int main(int argc, char** argv)
+{
+	// TODO...
+	return 0;
+}
